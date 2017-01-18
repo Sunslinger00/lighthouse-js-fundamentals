496563d1ef1b8b788c8e91fc02faaacf24b300cc
Levi <levineudorf@gmail.com>
2017-01-18 08:55:50 +0000

    Adds looping exercises

---
+++ ingredients.js
@@ -0,0 +1,22 @@
+var ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+
+// Write a while loop that prints out the contents of ingredients:
+
+var repeat = 0;
+while (repeat >10) {
+    console.log(ingredients);
+    repeat++;
+}
+console.log(ingredients);
+
+// Write a for loop that prints out the contents of ingredients:
+
+for (var i = 0; i < ingredients.length; i++) {
+  console.log(ingredients[i]);
+}
+
+// Write any loop (while or for) that prints out the contents of ingredients backwards:
+
+for (var i = 0; i < ingredients.length; i++) {
+  console.log(ingredients.reverse[i]);
+}
